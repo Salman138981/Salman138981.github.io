126c028a41b9274de1d928f2e9962e57393f0a41
salman khan <sk752547@gmail.com>
2023-06-21 03:21:16 +0530

    pdf error updated

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
+
 let body = document.querySelector('body');
 
 let sidemenu = document.getElementById("sidemenu");
 
-function openmenu(){
-    sidemenu.style.right= "0";
+function openmenu() {
+    sidemenu.style.right = "0";
 }
-
-function closemenu(){
+function closemenu() {
     sidemenu.style.right = "-200px";
 }
 
 //typing text animation script
-var typed = new Typed(".typing",{
-    strings : ["A Full Stack Developer", "A JAVA Backend Developer"],
+var typed = new Typed(".typing", {
+    strings: ["A Full Stack Developer",  "A JAVA Backend Developer"],
     typeSpeed: 80,
     backSpeed: 60,
-    loop: true
+    loop: true,
 })
 
-document.querySelector("#resume-link-1").addEventListener("click",()=>{
-    window.location.assign("https://mega.nz/file/Otl1GZAA#0GchG0dmbwLCN3-tZJqkpzKTtWOL8kdVRCG71iwcnRo","_blank");
+
+document.querySelector('#resume-link-1').addEventListener("click", () => {
+    // console.log("OPENinig.....")
+    window.location.assign("https://mega.nz/file/Otl1GZAA#0GchG0dmbwLCN3-tZJqkpzKTtWOL8kdVRCG71iwcnRo", "_blank");
 })
 
-document.querySelector("#resume-link-2").addEventListener("click",()=>{
-
-    window.location.assign("https://mega.nz/file/Otl1GZAA#0GchG0dmbwLCN3-tZJqkpzKTtWOL8kdVRCG71iwcnRo","_blank");
+document.querySelector('#resume-link-2').addEventListener("click", () => {
+    // console.log("OPENinig.....")
+    window.location.assign("https://mega.nz/file/Otl1GZAA#0GchG0dmbwLCN3-tZJqkpzKTtWOL8kdVRCG71iwcnRo", "_blank");
 })
-
 
 const sr = ScrollReveal({
     origin: 'left',
@@ -52,3 +53,5 @@ sr.reveal('#github-top-langs', { delay: 400 })
 sr.reveal('#github-stats-card', { delay: 600 })
 
  sr.reveal('.calendar', { delay: 600 })
+
+
