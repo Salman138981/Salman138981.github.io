2fbad80d6b72922c4a33169e8bc6e40ecb4ea9c0
salman khan <sk752547@gmail.com>
2023-06-20 23:19:32 +0530

    Javascript updated

--- index.js
+++ index.js
@@ -26,3 +26,29 @@ document.querySelector("#resume-link-2").addEventListener("click",()=>{
 
     window.location.assign("https://mega.nz/file/Otl1GZAA#0GchG0dmbwLCN3-tZJqkpzKTtWOL8kdVRCG71iwcnRo","_blank");
 })
+
+
+const sr = ScrollReveal({
+    origin: 'left',
+    distance: '120px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('#about h1', { delay: 200})
+sr.reveal('.home-img', { delay: 600 })
+sr.reveal('#user-detail-intro', { delay: 600 })
+sr.reveal('.skill', { delay: 200 })
+
+sr.reveal('.project', {})
+sr.reveal('.project-card img', { delay: 100 })
+sr.reveal('.project-title', { delay: 200 })
+sr.reveal('.project-description', { delay: 200 })
+sr.reveal('.project-tech-stack', { delay: 200 })
+sr.reveal('.repo-links', { delay: 200 })
+
+sr.reveal('#github-streak-stats', { delay: 200 })
+sr.reveal('#github-top-langs', { delay: 400 })
+sr.reveal('#github-stats-card', { delay: 600 })
+
+ sr.reveal('.calendar', { delay: 600 })
